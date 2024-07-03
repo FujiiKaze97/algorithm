c570a8e50ca1f3ce711932ea3e9d502fe85d4596
hongsw <64956234+room1333@users.noreply.github.com>
2024-07-03 15:07:10 +0900

    [level 1] Title: 정수 제곱근 판별, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/12934. 정수 제곱근 판별/정수 제곱근 판별.js
@@ -0,0 +1,15 @@
+function solution(n) {
+    var answer = 0;
+    
+    for(let i = 0;i<=n;i++) {
+        if(n / i == i) {
+            answer = (i+1) ** 2;
+            break;
+        }
+        else {
+            answer = -1;
+        }
+    }
+    
+    return answer;
+}

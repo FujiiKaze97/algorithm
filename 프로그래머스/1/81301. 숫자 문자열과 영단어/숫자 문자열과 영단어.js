6f3d8a512c5ae4c749109191a59f7d3368b12ada
hongsw <64956234+room1333@users.noreply.github.com>
2024-07-09 18:05:09 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.08 ms, Memory: 33.3 MB -BaekjoonHub

--- 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.js
+++ 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.js
@@ -1,60 +1,61 @@
 function solution(s) {
-    var answer = '';
+    var answer = 0;
     
     while(s.length!=0) {
         //첫 문자가 숫자인 경우 = answer에 문자열 추가 
+        var firstS  = s.substr(0,1);
         if(isNaN(s.substr(0,1))) {
             switch (s.charAt(0)) {
             case 'o' :
                 answer += '1';
-                s = s.slice(3);
+                s.slice(3);
                 break;
             case 't' :
                 if (s.substr(1,1)==='w') {
                     answer += '2';
-                    s = s.slice(3);
+                    s.slice(3);
                 } else {
                    answer += '3';
-                    s = s.slice(5);
+                    s.slice(5);
                 }
                 break;
             case 'f' :
                 if (s.substr(1,1)==='o') {
                     answer += '4';
-                    s = s.slice(4);
+                    s.slice(4);
                 } else {
                    answer += '5';
-                    s = s.slice(4);
+                    s.slice(4);
                 }
                 break;
             case 's' :
                 if (s.substr(1,1)==='i') {
                     answer += '6';
-                    s = s.slice(3);
+                    s.slice(3);
                 } else {
                    answer += '7';
-                    s = s.slice(5);
+                    s.slice(5);
                 }
                 break;
             case 'e' :
                     answer += '8';
-                    s = s.slice(5);
+                    s.slice(5);
                 break;
             case 'n' :
                     answer += '9';
-                    s = s.slice(4);
+                    s.slice(4);
                 break;
             case 'z' :
                     answer += '0';
-                    s = s.slice(4);
+                    s.slice(4);
                 break;
         }
         }
          else {
             //숫자일 경우
-            answer += s.substr(0,1);
-            s = s.slice(1);
+            answer += firstS;
+            s.slice(1);
         }
         }
-     return Number(answer);
+     return answer;
 }

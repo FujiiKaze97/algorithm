653ec72a92cb3d3dbdd9a6533cb8ed560a0b5d62
Fujii Kaze <64956234+FujiiKaze97@users.noreply.github.com>
2024-09-10 01:26:09 +0900

    [level 1] Title: 문자열 나누기, Time: 6.48 ms, Memory: 37.3 MB -BaekjoonHub

--- 프로그래머스/1/140108. 문자열 나누기/문자열 나누기.js
+++ 프로그래머스/1/140108. 문자열 나누기/문자열 나누기.js
@@ -1,23 +1,4 @@
 function solution(s) {
     var answer = 0;
-    var equalXCnt = 0;
-    var diffXCnt = 0;
-    var resultArr = [];
-    var x = 0;
-    
-
-        for(let i = 0; i<s.length;i++) {
-            if(s[i] === s[x]) {
-                equalXCnt++;
-            } else {
-                diffXCnt++;
-            } 
-
-            if(equalXCnt === diffXCnt) {
-                resultArr.push(s.slice(x,i+1))
-                x = i+1;
-            }
-    }
-    
-    return resultArr.join('').length === s.length ? resultArr.length : resultArr.length+1;
+    return answer;
 }
